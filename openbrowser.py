4b1065027a8a39b09d4bc15b1c96ab6dcfcf1294
Alan Tan <tanjunwei1@yahoo.com>
2018-07-04 13:12:12 -0700

    Update openbrowser.py

--- openbrowser.py
+++ openbrowser.py
@@ -4,7 +4,7 @@ import webbrowser
 total_breaks=3
 break_count=0
 
-print("The program start on  "+time.counttime())
+print("The program start on  "+time.ctime())
 while(break_count<total_breaks):
     time.sleep(10)
     webbrowser.open("http://www.youtube.com")
